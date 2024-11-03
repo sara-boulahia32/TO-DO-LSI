bcb007db1ccd9d71a52704cf46f5f84e6703c0f4
sara boulahia <saraboulahia32@gmail.com>
2024-11-03 14:11:58 +0100

    new repo

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        customCream: '#E6E6DC',
+        customGray: '#908787',
+        customBrown:'#CFC099',
+        customBlue:'#3E7CBA',
+        customGreen: '#6BB5B3',
+        customPink:'#C869C2',
+        customDarkBrown:'#88592D',
+      },
+    },
+  },
+  plugins: [],
+}
+
